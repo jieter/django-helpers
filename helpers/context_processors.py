7ca225955c3f90edd10ce9861501db41434c05a1
Jan Pieter Waagmeester <jieter@jieter.nl>
2015-12-03 13:19:13 +0100

    Remove google analytics context preprocessor

--- helpers/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-
-def googleanalytics(request):  # pragma: nocover
-    'Deprecated, use the templatetag in helpers.templatetags.analytics'
-    return {
-        'GOOGLE_ANALYTICS': getattr(settings, 'GOOGLE_ANALYTICS', None),
-    }
